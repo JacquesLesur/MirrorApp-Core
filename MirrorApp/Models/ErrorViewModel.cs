399d1b453581a17791c2bb663c68dbc91476d7c6
JacquesLesur <lesurjacques@gmail.com>
2019-01-09 10:16:59 +0100

    FirstPush

---
+++ MirrorApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MirrorApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
